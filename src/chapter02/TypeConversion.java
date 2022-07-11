6510bcf5bca89a37ab46ef568348771d364dbb0c
joyunkyung <dbsrud458@gmail.com>
2022-07-11 13:22:42 +0900

    aa

--- src/chapter02/TypeConversion.java
+++ src/chapter02/TypeConversion.java
@@ -10,8 +10,8 @@ public class TypeConversion {
 		System.out.println(10/4); //정수끼리 계산은 값도 정수
 		System.out.println(10.0/4); //10.0을 실수로 취급해서 값도 실수
 		System.out.println(2.9 + 1.8);
-		System.out.println((int)2.9 + 1.8); // (int)2.9=2
-		System.out.println((int)(2.9 + 1.8)); // (int)2.9=2
+		System.out.println((int)2.9 + 1.8); // (int) 2.9=2
+		System.out.println((int)(2.9 + 1.8)); // (int) 정수로 인식
 	}
 
 }
